0aecd7aa34795dd1616a9796df6fd8453f057631
IgorSantos <89170407+joseigors@users.noreply.github.com>
2022-10-19 09:52:50 -0300

    Update deps.py

---
+++ core/deps.py
@@ -0,0 +1,94 @@
+from typing import Generator, Optional
+from fastapi import Depends, HTTPException, status
+from jose import jwt, JWTError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from pydantic import BaseModel
+from core.database import Session
+from core.auth import oauth2_schema
+from core.config import settings
+from models.Usuario import UserModel
+from models.UserEnterprise import UserEnterprise
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+async def get_session() -> Generator:
+    session: AsyncSession = Session()
+
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
+async def get_current_user(db: Session = Depends(get_session), token: str = Depends(oauth2_schema)) -> UserModel:
+    credential_exception: HTTPException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Não foi possível autenticar a credencial',
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET,
+            algorithms=[settings.ALGORITHM],
+            options={"veryfy_aud": False}
+        )
+
+        username: str = payload.get("sub")
+
+        if username is None:
+            raise credential_exception
+
+        token_data: TokenData = TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+
+    async with db as session:
+        query = select(UserModel).filter(
+            UserModel.id == int(token_data.username))
+        result = await session.execute(query)
+        user: UserModel = result.scalars().unique().one_or_none()
+
+        if user is None:
+            raise credential_exception
+
+        return user
+
+
+# async def get_current_user_enterprise(db: Session = Depends(get_session), token: str = Depends(oauth2_schema)) -> UserEnterprise:
+#    credential_exception: HTTPException = HTTPException(
+#        status_code=status.HTTP_401_UNAUTHORIZED,
+#        detail='Não foi possível autenticar a credencial',
+#        headers={"WWW-Authenticate": "Bearer"},
+#    )
+#    try:
+#        payload = jwt.decode(
+#            token,
+#            settings.JWT_SECRET,
+#            algorithms=[settings.ALGORITHM],
+#            options={"veryfy_aud": False}
+#        )
+
+#        username: str = payload.get("sub")
+
+#        if username is None:
+#            raise credential_exception
+
+#        token_data: TokenData = TokenData(username=username)
+#    except JWTError:
+#        raise credential_exception
+
+#    async with db as session:
+#        query = select(UserEnterprise).filter(
+#            UserEnterprise.id == int(token_data.username))
+#        result = await session.execute(query)
+#        user_enterprise: UserEnterprise = result.scalars().unique().one_or_none()
+
+#        if user_enterprise is None:
+#            raise credential_exception
+
+#        return user_enterprise#
